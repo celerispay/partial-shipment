82ab4e604e45847561c0695d3fd4097126f68aae
Charchit Khandelwal <charchit@newgenpayments.com>
2021-03-19 19:50:50 +0530

    [Feature] Custom checkout step
    Signed-off-by: Charchit Khandelwal <charchit@newgenpayments.com>

---
+++ view/frontend/web/js/view/cart-page.js
@@ -0,0 +1,71 @@
+define(
+    [
+        'ko',
+        'uiComponent',
+        'underscore',
+        'Magento_Checkout/js/model/step-navigator',
+        'Magento_Customer/js/model/customer'
+    ],
+    function(
+        ko,
+        Component,
+        _,
+        stepNavigator,
+        customer
+    ) {
+        'use strict';
+        return Component.extend({
+            defaults: {
+                template: 'Boostsales_PartialShipment/cart-page'
+            },
+
+            //add here your logic to display step,
+            isVisible: ko.observable(true),
+            isLogedIn: customer.isLoggedIn(),
+            //step code will be used as step content id in the component template
+            stepCode: 'Cart',
+            //step title value
+            stepTitle: 'Shopping Cart',
+
+            /**
+             *
+             * @returns {*}
+             */
+            initialize: function() {
+                this._super();
+                // register your step
+                stepNavigator.registerStep(
+                    this.stepCode,
+                    //step alias
+                    null,
+                    this.stepTitle,
+                    //observable property with logic when display step or hide step
+                    this.isVisible,
+
+                    _.bind(this.navigate, this),
+
+                    9
+                );
+
+                return this;
+            },
+
+            /**
+             * The navigate() method is responsible for navigation between checkout step
+             * during checkout. You can add custom logic, for example some conditions
+             * for switching to your custom step
+             */
+            navigate: function() {
+
+            },
+
+
+            /**
+             * @returns void
+             */
+            navigateToNextStep: function() {
+                stepNavigator.next();
+            }
+        });
+    }
+);
